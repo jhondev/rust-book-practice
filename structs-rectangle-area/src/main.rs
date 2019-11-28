9e7e0bbaf5851837fa45153cd7e0d7c6b584dd5c
Jhonattan <jma9974@gmail.com>
2019-11-28 18:39:53 -0500

    structs, method syntax

--- structs-rectangle-area/src/main.rs
+++ structs-rectangle-area/src/main.rs
@@ -1,7 +1,21 @@
-#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, rect2: Rectangle) -> bool {
+        self.width > rect2.width && self.height > rect2.height
+    }
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -9,15 +23,15 @@ fn main() {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
-    println!("rect1 is {:?}", rect1);
-
-    println!(
-        "Area with structs - The area of the rectangle is {} sqare pixels",
-        area(&rect1)
-    )
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(rect3));
 }
